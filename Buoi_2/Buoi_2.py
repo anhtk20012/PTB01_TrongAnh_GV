62d6577e2a15c36995c469735ab623bdf491edbe
anhtk20012 <anhtk20012@gmail.com>
2023-06-10 19:46:58 +0700

    commit buổi 2

---
+++ Buoi_2/Buoi_2.py
@@ -0,0 +1,55 @@
+# Buổi sau thầy kiểm tra lại nội dung các bạn note
+
+# 1010101010111011 -> Ngôn ngữ máy tính
+
+# Ngôn ngữ bật cao -> python, C++, GML, ....
+
+# Cách chia nhị phân
+
+# print -> Hiển thị nội dung ra màn hình
+
+'''
+print("Trọng Ánh", 10, 20, 32, end= " ", sep= "------")
+print("10")
+print(12)
+'''
+
+# Nhiệm vụ hiển thị ra màn hình tên, tuổi, lớp, trường
+# Hiển thị theo dạng:
+'''
+Tên: Trọng Ánh
+Tuổi: 25
+Trường: MindX
+
+print("Tên:", "Trọng Ánh")
+print("Tuổi:", "25")
+print("Trường:", "MindX")
+'''
+
+'''
+a = 12.4
+print(type(a))
+'''
+
+'''
+Ten = "Ánh"
+Ho = "Lã"
+Dem = "Trọng"
+Ngay = "2"
+Thang = "4"
+Nam = "1998"
+Tuoi = "25"
+Truong = "MindX"
+
+print("Họ và tên: ", end="")
+print(Ho, Dem, Ten)
+
+print("Tuổi: ", end= "")
+print(Tuoi)
+
+print("Ngày sinh: ", end="")
+print(Ngay, Thang, Nam, sep="/")
+
+print("Trường học: ", end="")
+print(Truong)
+'''
