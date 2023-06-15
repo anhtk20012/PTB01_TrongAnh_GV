5bfdd4a0143bc4acc99a0a12d66bd277e8ec678e
anhtk20012 <anhtk20012@gmail.com>
2023-06-15 19:50:07 +0700

    commit BTVN buổi 2

---
+++ Buoi_2/BTVN2.py
@@ -0,0 +1,42 @@
+# Comment line - ghi chú
+'''
+Comment lines
+'''
+'''
+print("Tên: Trọng Ánh","Tuổi:", 10)
+print("Tên","Lã", "Trọng", "Ánh", sep=":")
+print("Tên:", end=" ") 
+print("Trọng Ánh")
+print("Tuổi:", end=" ")
+print(25)
+'''
+
+'''
+Variable -> local and global
+Datatype -> number, string, boolean
+Number -> integer -> nguyên, float -> thực
+String -> Chữ
+Boolean -> True, False
+'''
+Ten = "Phú Vinh"
+ten = ""
+Tuoi = 13
+Gioi_tinh = True
+
+print("Tên:",Ten)
+print("Tuổi:",2023 - Tuoi)
+print("Giới tính:",Gioi_tinh)
+
+# Quy tắc đặt tên biến
+'''
+Không bắt đầu bằng số
+Phân biệt chữ hoa và thường
+Không chứa kí tự đặc biệt
+Không có khoản trống có thể sử dụng _ làm khoảng trống
+......
+'''
+
+'''Tìm hiểu về cách nhập từ bàn phím thông 
+tin người dùng rồi lưu vào biến. 
+Hiển thị các thông tin ra màn hình.
+'''
